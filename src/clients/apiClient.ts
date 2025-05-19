e0c93a19abd3ab877caf2adbec34e3c69e05fa22
paulohfev <phfernandesev@gmail.com>
2025-05-19 08:07:17 -0300

    Add nextjs API route layer to hide token

--- src/clients/apiClient.ts
+++ src/clients/apiClient.ts
@@ -1,13 +1,9 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
-const PRODUCT_HUNT_API_URL = process.env.NEXT_PUBLIC_API_URL
-const token = process.env.NEXT_PUBLIC_API_TOKEN
-
 export const apolloClient = new ApolloClient({
   link: new HttpLink({
-    uri: PRODUCT_HUNT_API_URL,
+    uri: '/api/graphql',
     headers: {
-      Authorization: `Bearer ${token}`,
       'Content-Type': 'application/json',
     },
   }),
